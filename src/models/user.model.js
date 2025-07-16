956c696ca109a1e5165675406f6362a576860022
Hadi <969hadiahmed@gmail.com>
2025-07-16 17:58:04 +0530

    feat: User Model

---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default class User extends Model {
+  static associate(models) {
+    // this.belongsToMany(models.Skill, {
+    //   through: 'UserSkills',
+    //   foreignKey: 'userId',
+    // });
+
+    // this.hasMany(models.Project, {
+    //   foreignKey: 'userId',
+    // });
+  }
+}
+
+export const initUser = (sequelize) => {
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM('USER', 'ADMIN'),
+        defaultValue: 'USER',
+      },
+      bio: DataTypes.TEXT,
+      github: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+      timestamps: true,
+    }
+  );
+
+  return User;
+};
