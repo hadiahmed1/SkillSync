dd65eab8155e523f4d977ee9d2af8f95e5264708
Hadi <969hadiahmed@gmail.com>
2025-07-16 19:18:43 +0530

    feat: decodeToken

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import Boom from '@hapi/boom'
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = '1h';
@@ -7,4 +8,13 @@ export const generateJWT = (user) => {
     const payload = { id: user.id, email: user.email, role: user.email };
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
     return token;
+}
+
+export const decodeToken = (token) => {
+    if (!token) throw Boom.unauthorized('Missing access token');
+    try {
+        return jwt.verify(token, JWT_SECRET);
+    } catch (err) {
+        throw Boom.unauthorized('Invalid or expired token');
+    }
 }
