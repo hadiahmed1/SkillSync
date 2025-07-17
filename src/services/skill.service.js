4e2b5d0fb10eedf79ef6fb3d2fee057e06e1a502
Hadi Ahmed <969hadiahmed@gmail.com>
2025-07-17 16:06:56 +0530

    feat: service to remove userskill

--- src/services/skill.service.js
+++ src/services/skill.service.js
@@ -16,16 +16,6 @@ export const createSkill = async (name) => {
     return skill;
 }
 
-
-export const createUserSkill = async (userId, skillId) => {
-    const existingUserSkill = await db.UserSkill.findOne({ where: { userId, skillId } });
-    if (existingUserSkill) throw Boom.conflict("User already has this skill");
-
-    const user = await getUserByID(userId);
-    const skill = await getSkillByID(skillId);
-    user.addSkill(skill);
-}
-
 export const findAllSkills = async () => await db.Skill.findAll();
 
 export const findUserSkills = async (id) => {
@@ -38,3 +28,31 @@ export const findUserSkills = async (id) => {
 
     return user.Skills;
 }
+
+export const getUserSkillByIds = async (userId, skillId) => await db.UserSkill.findOne({ where: { userId, skillId } });
+
+
+export const createUserSkill = async (userId, skillId) => {
+    const existingUserSkill = getUserSkillByIds(userId, skillId);
+    if (existingUserSkill) throw Boom.conflict("User already has this skill");
+
+    const user = await getUserByID(userId);
+    const skill = await getSkillByID(skillId);
+    user.addSkill(skill);
+}
+
+export const removeUserSkill = async (userId, skillId) => {
+    const user = await db.User.findByPk(userId, {
+        include: {
+            model: db.Skill,
+            where: { id: skillId },
+            required: false,
+            through: { attributes: [] },
+        },
+    });
+
+    if (!user) throw Boom.notFound('User not found');
+    if (!user.Skills || user.Skills.length === 0) throw Boom.notFound('Skill not assigned to user');
+
+    await user.removeSkill(user.Skills[0]); // remove association
+};
