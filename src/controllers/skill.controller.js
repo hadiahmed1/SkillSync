7e03a98e516b6f93b617dfe8ec3131cc1a243ebf
Hadi <969hadiahmed@gmail.com>
2025-07-17 13:57:50 +0530

    fix: wrapping controllers in async handler

--- src/controllers/skill.controller.js
+++ src/controllers/skill.controller.js
@@ -1,38 +1,24 @@
-import { createSkill, createUserSkill } from "../services/skill.service.js";
+import { createSkill, createUserSkill, findAllSkills } from "../services/skill.service.js";
+import { handleAsync } from "../utils/asyncHandler.js";
 
-export const addSkill = async (request, h) => {
-  try {
-    const skill = await createSkill(request.payload.name);
-    return h.response({
-      message: 'Skill created successfully',
-      skill, addUserSkill
-    }).code(201);
-  } catch (err) {
-    if(err.isBoom) throw err;
-    return h.response({
-      statusCode: 400,
-      error: 'Bad Request',
-      message: err.message,
-    }).code(400);
-  }
-};
 
-export const addUserSkill = async (request, h) => {
-  try {
-    const user = request.auth.credentials;
-    const {skillId} = request.params;
+export const addSkill = handleAsync(async (request, h) => {
+  const skill = await createSkill(request.payload.name);
 
-    await createUserSkill(user.id, skillId);
+  return h.response({
+    message: 'Skill created successfully',
+    skill, addUserSkill
+  }).code(201);
+});
 
-    return h.response({
-      message: 'Skill added successfully',
-    }).code(201);
-  } catch (err) {
-    if(err.isBoom) throw err;
-    return h.response({
-      statusCode: 400,
-      error: 'Bad Request',
-      message: err.message,
-    }).code(400);
-  }
-};
+export const addUserSkill = handleAsync(async (request, h) => {
+  const user = request.auth.credentials;
+  const { skillId } = request.params;
+
+  await createUserSkill(user.id, skillId);
+
+  return h.response({
+    message: 'Skill added successfully',
+  }).code(201);
+});
+
