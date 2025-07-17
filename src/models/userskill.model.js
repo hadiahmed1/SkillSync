e5b9bed6e688150e8cc858c43e0e02cedcdc1733
Hadi <969hadiahmed@gmail.com>
2025-07-17 13:03:07 +0530

    fix: added id

--- src/models/userskill.model.js
+++ src/models/userskill.model.js
@@ -2,6 +2,11 @@ import { DataTypes } from "sequelize";
 
 export const initUserSkill = (sequelize) => {
   const UserSkill = sequelize.define('UserSkill', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     userId: {
       type: DataTypes.UUID,
       allowNull: false,
