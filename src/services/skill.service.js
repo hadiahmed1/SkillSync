69a5eceb05cff850c9d52cebd290ff9e9e0932c4
Hadi <969hadiahmed@gmail.com>
2025-07-17 11:46:01 +0530

    feat: skill services
    -getSkillByID: Find skill using ID else throw Boom Error
    -createSkill: create new skill, throw error if already exists
    -addUserSKill: associate user with a skill

---
+++ src/services/skill.service.js
@@ -0,0 +1,27 @@
+import db from '../models/index.js';
+import Boom from '@hapi/boom'
+import { getUserByID } from './user.service.js';
+
+export const getSkillByID = async (id)=>{
+    const skill = await db.Skill.findByPk(id);
+    if(skill) return skill;
+    throw Boom.notFound("Invalid skill ID")
+}
+
+export const createSkill = async (name) => {
+    const existingSkill = await db.Skill.findOne({ where: { name: name.toLowerCase() } })
+    if (existingSkill) throw Boom.conflict("Skill already exists");
+
+    const skill = await db.Skill.create({ name });
+    return skill;
+}
+
+
+export const addUserSkill = async (userId, skillId) => {
+    const existingUserSkill = await db.UserSkill.findOne({ where: { userId, skillId } });
+    if (existingUserSkill) throw Boom.conflict("User already has this skill");
+    
+    const user = await getUserByID(userId);
+    const skill = await getSkillByID(skillId);
+    user.addSkill(skill);
+}
